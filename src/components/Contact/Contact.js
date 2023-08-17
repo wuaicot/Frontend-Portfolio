6a1a7c3a269a47529802c6963b1d14cdc6ebbc05
RODO.L <wuaicot8@gmail.com>
2023-08-17 15:12:37 -0400

    aver

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
          //https://backend-portfolio-production-8985.up.railway.app/
          
       await axios.post(
-        "https://backend-portfolio-production-8985.up.railway.app/",
+        "http://localhost:5000/externed/contact'",
         formData
       );
       setShowSuccessMessage(true);
