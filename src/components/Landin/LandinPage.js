345551198bf56017cb7da7768708162132aac1d5
RODO.L <wuaicot8@gmail.com>
2023-08-27 21:58:21 -0400

    adding logo opensea in landin

--- src/components/Landin/LandinPage.js
+++ src/components/Landin/LandinPage.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 import styles from './LandinPage.module.css';
 import Matx from '../../image/Matx.png';
+import HeroSocial from "../HeroSocial/HeroSocial";
+import openLogo from "../../image/OpenSea_icon.svg1024.png";
 
+const open="https://opensea.io/es";
 const LandinPage = () => {
   // Define the inline style with the desired background color
   const containerStyle = {
@@ -16,6 +19,12 @@ const LandinPage = () => {
       
       <div className={styles.openS}>
         <p>un dos</p>
+        <HeroSocial
+          href={open}
+          src={openLogo}
+          alt="logo de linkedin"
+          title="LinkedIn"
+        />
       </div>
     </div>
   );
