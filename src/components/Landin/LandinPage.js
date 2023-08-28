053a292b7dca6c987e94c02ae4cb370172f78f60
RODO.L <wuaicot8@gmail.com>
2023-08-28 14:45:52 -0400

    adding fswd

--- src/components/Landin/LandinPage.js
+++ src/components/Landin/LandinPage.js
@@ -4,6 +4,7 @@ import styles from './LandinPage.module.css';
 import Matx from '../../image/Matx.png';
 import HeroSocial from "../HeroSocial/HeroSocial";
 import openLogo from "../../image/OpenSea_icon.svg1024.png";
+import FSWD from "../../image/Certificado_Henry_FSWD.png";
 
 const open="https://opensea.io/es/wuaicotnft22";
 const LandinPage = () => {
@@ -12,6 +13,8 @@ const LandinPage = () => {
     backgroundImage: `url(${Matx})`, // Set the background image here
     backgroundSize: 'cover',
   };
+
+   const certFswd= "https://certificates.soyhenry.com/new-cert?id=8173422a2e645676511f3fa24f6db9174d81e3e76871bed6da857307da8a6f0b";
 
   return (
     <div className={styles.container} style={containerStyle}>
@@ -25,8 +28,21 @@ const LandinPage = () => {
           alt="logo de openSea"
           title="WUAICOTNFT22"
         />
+
+         
       </div>
+
+      <div>
+        <HeroSocial             
+       src={FSWD}
+       href={certFswd}
+       alt="miniatura del certificado FSWD"
+       title="Full Stack Web Developer_Naycol R. Linares V."
+      />
+      </div>
+      
     </div>
+    
   );
 };
 
