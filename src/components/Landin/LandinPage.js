26697ef6904e681f1b17a94a956af6f7edfee2af
RODO.L <wuaicot8@gmail.com>
2023-08-28 16:49:25 -0400

    adding TA certificate in landinpage

--- src/components/Landin/LandinPage.js
+++ src/components/Landin/LandinPage.js
@@ -5,8 +5,13 @@ import Matx from '../../image/Matx.png';
 import HeroSocial from "../HeroSocial/HeroSocial";
 import openLogo from "../../image/OpenSea_icon.svg1024.png";
 import FSWD from "../../image/Certificado_Henry_FSWD.png";
+import taCertf from "../../image/TA-Certf1252x877.png";
 
 const open="https://opensea.io/es/wuaicotnft22";
+const certFswd= "https://certificates.soyhenry.com/new-cert?id=8173422a2e645676511f3fa24f6db9174d81e3e76871bed6da857307da8a6f0b";
+const TA= "https://certificates.soyhenry.com/new-cert?id=5e76160462ff0372ef65f37267628f90d0a9324febcc435d947247098c6d2ca4";
+
+
 const LandinPage = () => {
   // Define the inline style with the desired background color
   const containerStyle = {
@@ -14,7 +19,7 @@ const LandinPage = () => {
     backgroundSize: 'cover',
   };
 
-   const certFswd= "https://certificates.soyhenry.com/new-cert?id=8173422a2e645676511f3fa24f6db9174d81e3e76871bed6da857307da8a6f0b";
+   
 
   return (
     <div className={styles.container} style={containerStyle}>
@@ -28,17 +33,23 @@ const LandinPage = () => {
           alt="logo de openSea"
           title="WUAICOTNFT22"
         />
-
+        
          
       </div>
 
-      <div>
-        <HeroSocial             
+      <div className={styles.certifFswd}>
+      <HeroSocial             
        src={FSWD}
        href={certFswd}
        alt="miniatura del certificado FSWD"
        title="Full Stack Web Developer_Naycol R. Linares V."
       />
+      <HeroSocial 
+          href={TA}
+          src={taCertf}
+          alt="TEACHING ASSISTANT"
+          title="Mi certificado TA"
+        /> 
       </div>
       
     </div>
@@ -48,4 +59,4 @@ const LandinPage = () => {
 
 export default LandinPage;
 
-
+        
