00d9aacc8cf77f1cf2ebefef767fe8edb6ee3d70
RODO.L <wuaicot8@gmail.com>
2023-08-27 22:26:55 -0400

    was added logoOpenSea in landin

--- src/components/Landin/LandinPage.js
+++ src/components/Landin/LandinPage.js
@@ -5,7 +5,7 @@ import Matx from '../../image/Matx.png';
 import HeroSocial from "../HeroSocial/HeroSocial";
 import openLogo from "../../image/OpenSea_icon.svg1024.png";
 
-const open="https://opensea.io/es";
+const open="https://opensea.io/es/wuaicotnft22";
 const LandinPage = () => {
   // Define the inline style with the desired background color
   const containerStyle = {
@@ -18,12 +18,12 @@ const LandinPage = () => {
       <h1 className={styles.acheuno}>Gacias por visitar mi portafolio digital</h1>
       
       <div className={styles.openS}>
-        <p>un dos</p>
+        <p className={styles.tgTr}>¿te gustaría ver algunas imagenes de mi galería artistica?</p>
         <HeroSocial
           href={open}
           src={openLogo}
-          alt="logo de linkedin"
-          title="LinkedIn"
+          alt="logo de openSea"
+          title="OpenSea"
         />
       </div>
     </div>
