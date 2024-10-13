e2ec92be825dbf6676b6dbbaa395be4ec209e5c8
Naycol Rodolfo Linares Villasmil <wuaicot8@gmail.com>
2024-10-12 21:26:03 -0300

    Changing Language Projects and Added Flags Build

--- src/components/ProjectsCards/ProjectsCards.js
+++ src/components/ProjectsCards/ProjectsCards.js
@@ -6,19 +6,19 @@ const projects = [
   {
     id: 1,
     title: 'The Literary Corner',
-    description: 'An "e-commerce" in which the user, a lover of books and literature, will be able to choose from hundreds of books with the possibility of viewing the details, adding to favorites, making reviews, adding to the shopping cart, buying books and further',
+    description: 'Un "e-commerce" en el que el usuario, amante de los libros y la literatura, podrá elegir entre cientos de libros con la posibilidad de ver los detalles, añadir a favoritos, hacer reseñas, añadir al carrito de la compra, comprar libros y mucho más',
     deployedUrl: 'https://pf-henry-bookstore.vercel.app/',
   },
   {
     id: 2,
     title: 'Portal Info-Dogs',
-    description: 'It is a SAP (single page application) created with the purpose of obtaining information on the various breeds of dogs from an external API. The user can search, filter, sort and create their dog breed.',
+    description: 'Se trata de una SAP (aplicación de página única) creada con el propósito de obtener información sobre las distintas razas de perros a partir de una API externa. El usuario puede buscar, filtrar, clasificar y crear su raza de perro.',
     deployedUrl: 'https://portal-info-dogs.vercel.app/',
   },
   {
     id: 3,
     title: 'Portal Video-Game',
-    description: 'Here soon you will be able to create your favorite collection of video games',
+    description: 'Aquí pronto podrás crear tu colección favorita de videojuegos',
     deployedUrl: 'https://pi-videogames-main-mi-ihac35zgc-wuaicot.vercel.app/',
   },
   {
@@ -29,10 +29,10 @@ const projects = [
   },
   {
     id: 5,
-    title: 'Soon',    
-    description: 'still in developmentt add .',
+    title: 'Soon',
+    description: 'Aún en desarrollo.',
     deployedUrl: 'http://www.enespera.com',
-    
+
   }
   // Agrega más proyectos si es necesario
 ];
@@ -41,7 +41,7 @@ const ProjectsCards = () => {
   return (
     <div className={styles.container}>
       <div className={styles.adverting}>
-      <h1>These are some of my personal projects.</h1>
+        <h1>Estos son algunos de mis proyectos personales.</h1>
       </div>
       <div className={styles.cardsContainer}>
         {projects.map((project) => (
@@ -49,7 +49,7 @@ const ProjectsCards = () => {
             <h2>{project.title}</h2>
             <p>{project.description}</p>
             <a href={project.deployedUrl} target="_blank" rel="noopener noreferrer">
-            See Project
+              See Project
             </a>
           </div>
         ))}
