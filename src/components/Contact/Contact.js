50000e26949ef38fbee5e2e474624e9fd0ead677
Naycol Linares <wuaicot8@gmail.com>
2024-10-29 23:44:46 -0300

    styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://backend-portfolio-production-737e.up.railway.app/externed/contact", formData);
+      const response = await axios.post("https://backend-portfolio-production-737e.up.railway.app", formData);
       console.log('Respuesta del servidor:', response.data); // Asegúrate de que la respuesta se reciba correctamente
       openModal(); // Abre el modal en caso de éxito
       setFormData({ name: "", email: "", message: "" });
