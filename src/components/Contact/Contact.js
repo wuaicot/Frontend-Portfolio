9aee8baba32de1c1f040fa21a61df23061ab7dc1
Naycol Linares <wuaicot8@gmail.com>
2024-10-30 01:44:07 -0300

    3000

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,6 +8,7 @@ import Modal from "react-modal";
 
 Modal.setAppElement('#root');
 
+
 const cellPhone = "https://api.whatsapp.com/send/?phone=5693380684&text&app_absent=0";
 const email = "https://mail.google.com/mail/wuaicot8@gmail.com";
 
@@ -24,6 +25,7 @@ const Contact = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -32,37 +34,37 @@ const Contact = () => {
     setIsModalOpen(false);
   };
 
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
+ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${BACKEND_URL}/externed/contact`, formData);
+ const response = await axios.post(`${BACKEND_URL}/externed/contact`, formData);
       openModal(); 
       setFormData({ name: "", email: "", message: "" });
     } catch (error) {
-      if (error.response && error.response.status !== 500) {
+
+ if (error.response && error.response.status !== 500) {
         alert("¡Disculpa! En este momento no es posible enviar el mensaje. Puedes contactarme directamente en mi correo electrónico o Whatsapp. ¡Gracias!");
       } else {
         console.error("Error en el servidor:", error);
       }
     }
   };
-
-  return (
+ return 
     <div className={styles.container}>
       <div className={styles.Estoy}>
         <h4>
-          "Estoy listo para comenzar un nuevo proyecto y ayudarte a llevar a cabo
-          Tu idea. Contáctame <span role="img" aria-label="cara sonriente">😊</span>"
+          "Estoy listo para comenzar un nuevo proyecto y ayudarte a llevar a caboTu idea. Contáctame <span role="img" aria-label="cara sonriente">😊</span>"
         </h4>
       </div>
+      
       <br></br>
 
-      <form onSubmit={handleSubmit}>
+<form onSubmit={handleSubmit}>
         <div className={styles.tnc}>
           <label htmlFor="name">tu nombre completo:</label>
-          <input
+ <input
             type="text"
             id="name"
             name="name"
@@ -72,8 +74,8 @@ const Contact = () => {
             required
           />
         </div>
-        
-        <hr></hr>
+
+  <hr></hr>
         <div className={styles.tem}>
           <label htmlFor="email">tu email:</label>
           <input
@@ -95,51 +97,16 @@ const Contact = () => {
             name="message"
             value={formData.message}
             onChange={handleChange}
-            placeholder="📒 describe tu idea aquí"
-            required
-          ></textarea>
-        </div>
+            placeholder="📒 describe tu idea aquí">
+            required 
+            
+            </textarea>
+            </div>
+            </form>
+            </div>
 
-        <button className={styles.Enviar} type="submit">Enviar</button>
-        
-        <div className={styles.whatsapp}>
-          <HeroSocial
-            src={whatsappLogo}
-            alt="phone-Sticker"
-            title="+56963380684"
-            header="WhatsApp"
-            href={cellPhone}
-            target="_blank"
-            linkName="Enlace al Chat"
-          />
-        </div>
+}
 
-        <div className={styles.gmail}>
-          <HeroSocial
-            src={gmailLogo}
-            alt="phone-Sticker"
-            title="wuaicot8@gmail.com"
-            header="gmail"
-            href={email}
-            target="_blank"
-            linkName="wuaicot8@gmail.com"
-          />
-        </div>
-      </form>
-
-      {/* Modal para mostrar el mensaje de éxito */}
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Message sent"
-        className={styles.successModal}
-      >
-        <h2>Thank you. Your message has been sent!</h2>
-        <p>I will contact you soon</p>
-        <button onClick={closeModal}>Close</button>
-      </Modal>
-    </div>
-  );
-};
+            
 
 export default Contact;
