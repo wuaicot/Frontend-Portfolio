ccbc953f4b9151cec97bee80a1a13aa0f0e3b2d6
{username} <wuaicot8@gmail.com>
2025-01-06 19:52:03 -0300

    modifying projectcard

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,9 +6,10 @@ import gmailLogo from "../../image/gmail.png";
 import whatsappLogo from "../../image/logoWhatsap.png";
 import Modal from "react-modal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
-const cellPhone = "https://api.whatsapp.com/send/?phone=5693380684&text&app_absent=0";
+const cellPhone =
+  "https://api.whatsapp.com/send/?phone=5693380684&text&app_absent=0";
 const email = "https://mail.google.com/mail/wuaicot8@gmail.com";
 
 const Contact = () => {
@@ -32,17 +33,23 @@ const Contact = () => {
     setIsModalOpen(false);
   };
 
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
+  const BACKEND_URL =
+    process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${BACKEND_URL}/externed/contact`, formData);
-      openModal(); 
+      await axios.post(
+        `${BACKEND_URL}/externed/contact`,
+        formData
+      );
+      openModal();
       setFormData({ name: "", email: "", message: "" });
     } catch (error) {
       if (error.response && error.response.status !== 500) {
-        alert("¡Disculpa! En este momento no es posible enviar el mensaje. Puedes contactarme directamente en mi correo electrónico o Whatsapp. ¡Gracias!");
+        alert(
+          "¡Disculpa! En este momento no es posible enviar el mensaje. Puedes contactarme directamente en mi correo electrónico o Whatsapp. ¡Gracias!"
+        );
       } else {
         console.error("Error en el servidor:", error);
       }
@@ -53,8 +60,12 @@ const Contact = () => {
     <div className={styles.container}>
       <div className={styles.Estoy}>
         <h4>
-          "Estoy listo para comenzar un nuevo proyecto y ayudarte a llevar a cabo
-          Tu idea. Contáctame <span role="img" aria-label="cara sonriente">😊</span>"
+          "Estoy listo para comenzar un nuevo proyecto y ayudarte a llevar a
+          cabo Tu idea. Contáctame{" "}
+          <span role="img" aria-label="cara sonriente">
+            😊
+          </span>
+          "
         </h4>
       </div>
       <br></br>
@@ -72,7 +83,7 @@ const Contact = () => {
             required
           />
         </div>
-        
+
         <hr></hr>
         <div className={styles.tem}>
           <label htmlFor="email">tu email:</label>
@@ -90,7 +101,7 @@ const Contact = () => {
 
         <div className={styles.tmsj}>
           <label htmlFor="message">tu mensaje:</label>
-          <textarea 
+          <textarea
             id="message"
             name="message"
             value={formData.message}
@@ -100,8 +111,10 @@ const Contact = () => {
           ></textarea>
         </div>
 
-        <button className={styles.Enviar} type="submit">Enviar</button>
-        
+        <button className={styles.Enviar} type="submit">
+          Enviar
+        </button>
+
         <div className={styles.whatsapp}>
           <HeroSocial
             src={whatsappLogo}
